845f9203de1f345eee520c2bb4d6716114c8f808
101003107 <dingfan@huaqin.com>
2017-10-26 16:07:44 +0800

    Initial commit

---
+++ app/src/main/java/com/dfan/mylayout/MainActivity.java
@@ -0,0 +1,202 @@
+package com.dfan.mylayout;
+
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.dfan.mylayout.bottomview.BottomBarItem;
+import com.dfan.mylayout.bottomview.BottomBarLayout;
+import com.dfan.mylayout.fragment.TabFragment;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mVpContent;
+    private BottomBarLayout mBottomBarLayout;
+
+    private List<TabFragment> mFragmentList = new ArrayList<>();
+    private RotateAnimation mRotateAnimation;
+    private Handler mHandler = new Handler();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+        initListener();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void initView() {
+        mVpContent = (ViewPager) findViewById(R.id.vp_content);
+        mBottomBarLayout = (BottomBarLayout) findViewById(R.id.bbl);
+    }
+
+    private void initData() {
+
+        TabFragment homeFragment = new TabFragment();
+        Bundle bundle1 = new Bundle();
+        bundle1.putString(TabFragment.CONTENT, "首页");
+        homeFragment.setArguments(bundle1);
+        mFragmentList.add(homeFragment);
+
+        TabFragment videoFragment = new TabFragment();
+        Bundle bundle2 = new Bundle();
+        bundle2.putString(TabFragment.CONTENT, "视频");
+        videoFragment.setArguments(bundle2);
+        mFragmentList.add(videoFragment);
+
+        TabFragment microFragment = new TabFragment();
+        Bundle bundle3 = new Bundle();
+        bundle3.putString(TabFragment.CONTENT, "微头条");
+        microFragment.setArguments(bundle3);
+        mFragmentList.add(microFragment);
+
+        TabFragment meFragment = new TabFragment();
+        Bundle bundle4 = new Bundle();
+        bundle4.putString(TabFragment.CONTENT, "我的");
+        meFragment.setArguments(bundle4);
+        mFragmentList.add(meFragment);
+    }
+
+    private void initListener() {
+        mVpContent.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        mBottomBarLayout.setViewPager(mVpContent);
+        mBottomBarLayout.setOnItemSelectedListener(new BottomBarLayout.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final BottomBarItem bottomBarItem, int position) {
+                if (position == 0) {
+                    //如果是第一个，即首页
+                    if (mBottomBarLayout.getCurrentItem() == position) {
+                        //如果是在原来位置上点击,更换首页图标并播放旋转动画
+                        bottomBarItem.setIconSelectedResourceId(R.mipmap.tab_loading);//更换成加载图标
+                        bottomBarItem.setStatus(true);
+
+                        //播放旋转动画
+                        if (mRotateAnimation == null) {
+                            mRotateAnimation = new RotateAnimation(0, 360,
+                                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                                    0.5f);
+                            mRotateAnimation.setDuration(800);
+                            mRotateAnimation.setRepeatCount(-1);
+                        }
+                        ImageView bottomImageView = bottomBarItem.getImageView();
+                        bottomImageView.setAnimation(mRotateAnimation);
+                        bottomImageView.startAnimation(mRotateAnimation);//播放旋转动画
+
+                        //模拟数据刷新完毕
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                bottomBarItem.setIconSelectedResourceId(R.mipmap.tab_home_selected);//更换成首页原来图标
+                                bottomBarItem.setStatus(true);//刷新图标
+                                cancelTabLoading(bottomBarItem);
+                            }
+                        }, 3000);
+                        return;
+                    }
+                }
+
+                //如果点击了其他条目
+                BottomBarItem bottomItem = mBottomBarLayout.getBottomItem(0);
+                bottomItem.setIconSelectedResourceId(R.mipmap.tab_home_selected);//更换为原来的图标
+
+                cancelTabLoading(bottomItem);//停止旋转动画
+            }
+        });
+
+        mBottomBarLayout.setUnread(0, 20);//设置第一个页签的未读数为20
+        mBottomBarLayout.setUnread(1, 101);//设置第二个页签的未读书
+        mBottomBarLayout.showNotify(2);//设置第三个页签显示提示的小红点
+        mBottomBarLayout.setMsg(3, "NEW");//设置第四个页签显示NEW提示文字
+    }
+
+    /**
+     * 停止首页页签的旋转动画
+     */
+    private void cancelTabLoading(BottomBarItem bottomItem) {
+        Animation animation = bottomItem.getImageView().getAnimation();
+        if (animation != null) {
+            animation.cancel();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.dfan.mylayout/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.dfan.mylayout/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    class MyAdapter extends FragmentStatePagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+    }
+}
